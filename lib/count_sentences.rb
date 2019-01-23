7f09e5fcc95ff1aec1ffae1b4f839a94f9ce7278
Mitchel Parish <mitchelparish@gmail.com>
2019-01-23 00:20:22 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/[[:punct:]]/).count
+    self.split(/[[:punct:]]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
